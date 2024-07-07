e349b213d28e9bcf350c0abcfdb1eb8d6e73430c
mlpz <miguelkrasnob@gmail.com>
2024-07-07 19:13:08 +0200

    feat: Add BlogComponent and import FormsModule, and favicon.ico change

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
 
 }
+
